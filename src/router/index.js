48654a30e567c0dada176928af93c857c6d895b8
Пользователь <timurkazah1998@gmail.com>
2021-03-15 19:21:38 +0500

    header home sclad exp done

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/Sclad',
-    name: 'Sclad',
-    component: () => import('../views/Sclad.vue')
-  },
-{
-    path: '/Static',
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/Sclad',
+        name: 'Sclad',
+        component: () => import('../views/Sclad.vue')
+    },
+    {
+        path: '/Static',
         name: 'Static',
-    component: () => import('../views/Static.vue')
-},
+        component: () => import('../views/Static.vue')
+    },
     {
         path: '/Add',
         name: 'Add',
         component: () => import('../views/Add.vue')
     },
-]
+    {
+        path: '/Exp',
+        name: 'Exp',
+        component: () => import('../views/Exp.vue')
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
